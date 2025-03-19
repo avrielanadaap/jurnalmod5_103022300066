1ea197b273dce14b76ecc110bad4110319dec2aa
avrielanadaap <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 08:08:21 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using mod5_103022300066;
+using modul5_103022300066;
+public class program
+{
+    public static void Main(string[] args)
+    {
+        pemrosesData pemroses = new pemrosesData();
+        pemroses.DapatkanNilaiTerbesar<int>(30, 00, 66);
+
+        simpleDatabase<int> data = new simpleDatabase<int>();
+        data.addData(30);
+        data.addData(00);
+        data.addData(66);
+        data.printaAllData();
+    }
+}
---
+++ simpleDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103022300066
+{
+    class simpleDatabase<T>
+    {
+        public List<T> storedData;
+        public List<DateTime> inputDate;
+
+        public simpleDatabase()
+        {
+            storedData = new List<T>();
+            inputDate = new List<DateTime>();
+        }
+
+        public void addData(T data)
+        {
+            storedData.Add(data);
+            inputDate.Add(DateTime.Now);
+        }
+
+        public void printaAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data: " + storedData[i] + " | Date: " + inputDate[i]);
+            }
+        }
+    }
+}
